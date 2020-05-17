b409239ec8a4d2f44e42dc9d5c8d878fd5a990a1
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-17 09:45:59 -0700

    SSR: Render UsersList. Make use of redux store for client side

---
+++ 12-server-side-rendering/server/src/client/components/UsersList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUsers } from '../actions';
+
+class UsersList extends React.Component {
+  componentDidMount() {
+    this.props.fetchUsers();
+  }
+
+  renderUsers() {
+    return this.props.users.map(user => <li key={user.id}>{user.name}</li>);
+  }
+
+  render() {
+    return (
+      <div>
+        Here's a big list of users
+        <ul>{this.renderUsers()}</ul>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
