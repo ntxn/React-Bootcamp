8544bbbca27b6471e979ac1c754f04d203457c38
Kiera Nguyen <ngannguyen9017@gmail.com>
2020-05-18 12:26:43 -0700

    SSR: Add SEO support by using react-helmet on the server side

--- 12-server-side-rendering/server/src/client/pages/UsersListPage.js
+++ 12-server-side-rendering/server/src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchUsers } from '../actions';
+import { Helmet } from 'react-helmet';
 
 class UsersList extends React.Component {
   componentDidMount() {
@@ -11,9 +12,19 @@ class UsersList extends React.Component {
     return this.props.users.map(user => <li key={user.id}>{user.name}</li>);
   }
 
+  head() {
+    return (
+      <Helmet>
+        <title>{`${this.props.users.length} Users Loaded`}</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    );
+  }
+
   render() {
     return (
       <div>
+        {this.head()}
         Here's a big list of users
         <ul>{this.renderUsers()}</ul>
       </div>
